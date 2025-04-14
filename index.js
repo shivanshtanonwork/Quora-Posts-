7245ba5fa00b73b5c2cff167ec5310df4dfdb414
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-04-14 19:30:02 +0530

    index route

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const app = express();
+const port = 3000;
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }))
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
+
+app.use(express.static(path.join(__dirname, "public")))
+
+let posts = [
+    {
+        username: "shivanshtandon",
+        content: "I love coding"
+    },
+    {
+        username: "vidushichoubey",
+        content: "I love data"
+    },
+    {
+        username: "blackcaps",
+        content: "I love gaming"
+    },
+]
+
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", { posts })
+
+})
+
+app.listen(port, () => {
+    console.log(`Listening on localhost:${port}`);
+})
